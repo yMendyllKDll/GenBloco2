7bc8a324c27b65b061f355f92128a6695bb8ccd6
Anderson <and_mendescosta@hotmail.com>
2022-03-23 11:41:48 -0300

    Endpoints

--- Spring/Hello/src/main/java/com/helloword/Hello/controller/HelloController.java
+++ Spring/Hello/src/main/java/com/helloword/Hello/controller/HelloController.java
@@ -5,11 +5,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
+
 public class HelloController {
 	
-		@GetMapping
+		@GetMapping("/hello")
 		public String hello() {
-			return "Hello Generation!!!";
+			return "Hello world";
+		}
+		
+		@GetMapping("/bsm")
+		public String bsm() {
+			return "Orientação ao Futuro; \n Responsabilidade Pessoal; \n Mentalidade de Crescimento; \n"
+					+ "Persistência; \n Trabalho em Equipe; \n Atenção aos Detalhes; \n Proatividade; \n Comunicação; \n";
+		}
+		
+		@GetMapping("/objetivos")
+		public String objetivos() {
+			return "Aprender Spring";
 		}
 }
